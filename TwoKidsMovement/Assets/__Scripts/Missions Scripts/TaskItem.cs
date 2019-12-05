50267ee6c913168108f2019a13b6828786669eae
bidigame <elad0211@gmail.com>
2019-12-05 22:53:58 +0200

    can speaklonger with "while" in game

--- TwoKidsMovement/Assets/__Scripts/Missions Scripts/TaskItem.cs
+++ TwoKidsMovement/Assets/__Scripts/Missions Scripts/TaskItem.cs
@@ -2,70 +2,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class TaskItem : MonoBehaviour
+namespace VikingCrewTools.UI
 {
-    public string taskText;
-
-    public Collider destination;
-
-    public bool active;
-
-    public bool Finished = false;
-
-   public  bool talkTofinish;
-
-    public int ID;
-
-    public event Action thisTaskStarted;
-
-    public event Action thisTaskFinished;
-
-    public UI_Maneger UI = UI_Maneger.instance;
-
-    private void Awake()
+    public class TaskItem : MonoBehaviour
     {
-    }
+        public string taskText;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (active && destination == other)
-        {if (UI == null)
-                Debug.Log("fuck");
-            UI.MarkTask(ID);
-            active = false;
-            Debug.Log(taskText);
-            OnThisTaskFinish();
-        }
-    }
+        public Collider destination;
 
-    public void Intercacted()
-    {
-        if (active && talkTofinish)
+        public bool active;
+
+        public bool Finished = false;
+
+        public bool talkTofinish;
+
+        public int ID;
+
+        public event Action thisTaskStarted;
+
+        public event Action thisTaskFinished;
+
+        public UI_Maneger UI = UI_Maneger.instance;
+
+        private int x;
+
+        private void Awake()
         {
-            UI.MarkTask(ID);
-            active = false;
-            Debug.Log(taskText);
-            OnThisTaskFinish();
-            
+            x = GetComponent<ScriptedDialogueBehaviour>().scriptWhile.Length;
         }
-    }
 
-
-    private void OnThisTaskFinish()
-    {
-        if (thisTaskFinished !=null)
+        private void OnTriggerEnter(Collider other)
         {
-            Finished = true;
-            thisTaskFinished();
+            if (active && destination == other)
+            { if (UI == null)
+                    Debug.Log("fuck");
+                UI.MarkTask(ID);
+                active = false;
+                Debug.Log(taskText);
+                OnThisTaskFinish();
+            }
         }
-    }
-    public void OnThisTaskStarted()
-    {
-        if (thisTaskStarted != null)
-        {
-            thisTaskStarted();
-        }
-    }
 
+        public void Intercacted()
+        {
+            if (active && talkTofinish)
+            {
+                x--;
+                if (x <= 0)
+                {
+                    UI.MarkTask(ID);
+                    active = false;
+                    Debug.Log(taskText);
+                    OnThisTaskFinish();
+                }
+
+            }
+        }
+
+
+        private void OnThisTaskFinish()
+        {
+            if (thisTaskFinished != null)
+            {
+                Finished = true;
+                thisTaskFinished();
+            }
+        }
+        public void OnThisTaskStarted()
+        {
+            if (thisTaskStarted != null)
+            {
+                thisTaskStarted();
+            }
+        }
+
+    }
 }
